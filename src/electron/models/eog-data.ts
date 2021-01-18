d6197a3387fe66185d6db21a6ba4180d7ded1a4b
Adam <xadamxk@gmail.com>
2021-01-18 13:34:31 -0600

    Created models for EOG event data

---
+++ src/electron/models/eog-data.ts
@@ -0,0 +1,85 @@
+import { Model } from '../api';
+import { EOGTeam } from './eog-team';
+
+export class EOGData extends Model {
+  "accountId": number;
+  "basePoints": number;
+  "battleBoostIpEarned": number;
+  "boostIpEarned": number;
+  "boostXpEarned": number;
+  "causedEarlySurrender": boolean;
+  "championId": number;
+  "coOpVsAiMinutesLeftToday": number;
+  "coOpVsAiMsecsUntilReset": number;
+  "completionBonusPoints": number;
+  "currentLevel": number;
+  "customMinutesLeftToday": number;
+  "customMsecsUntilReset": number;
+  "difficulty": string;
+  "earlySurrenderAccomplice": boolean;
+  "elo": number;
+  "eloChange": number;
+  "experienceEarned": number;
+  "experienceTotal": number;
+  "firstWinBonus": number;
+  "gameEndedInEarlySurrender": boolean;
+  "gameId": number;
+  "gameLength": number;
+  "gameMode": string;
+  "gameMutators": string[];
+  "gameType": string;
+  "globalBoostXpEarned": number;
+  "imbalancedTeamsNoPoints": boolean;
+  "invalid": boolean;
+  "ipEarned": number;
+  "ipTotal": number;
+  "leveledUp": boolean;
+  "loyaltyBoostIpEarned": number;
+  "loyaltyBoostXpEarned": number;
+  "missionsXpEarned": number;
+  "myTeamStatus": string;
+  "newSpells": any[]; // TODO
+  "nextLevelXp": number;
+  "odinBonusIp": number;
+  "partyRewardsBonusIpEarned": number;
+  "pointsPenalties": any; // TODO
+  "preLevelUpExperienceTotal": number;
+  "preLevelUpNextLevelXp": number;
+  "previousLevel": number;
+  "previousXpTotal": number;
+  "queueBonusEarned": number;
+  "queueType": string;
+  "ranked": boolean;
+  "reportGameId": number;
+  "rerollData": {
+    "pointChangeFromChampionsOwned": number;
+    "pointChangeFromGameplay": number;
+    "pointsUntilNextReroll": number;
+    "pointsUsed": number;
+    "previousPoints": number;
+    "rerollCount": number;
+    "totalPoints": number;
+  };
+  "roomName": string;
+  "roomPassword": string;
+  "rpEarned": number;
+  "sendStatsToTournamentProvider": boolean;
+  "skinId": number;
+  "skinIndex": number;
+  "summonerId": number;
+  "summonerName": string;
+  "talentPointsGained": number;
+  "teamBoost": {
+    "availableSkins": any[]; // TODO
+    "ipReward": number;
+    "ipRewardForPurchaser": number;
+    "price": number;
+    "skinUnlockMode": string;
+    "summonerName": string;
+    "unlocked": boolean;
+  };
+  "teamEarlySurrendered": boolean;
+  "teams": EOGTeam[];
+  "timeUntilNextFirstWinBonus": number;
+  "userId": number;
+}
---
+++ src/electron/models/eog-player.ts
@@ -0,0 +1,29 @@
+import { Model } from '../api';
+import { EOGStats } from "./eog-stats";
+
+export class EOGPlayer extends Model {
+  "botPlayer": boolean;
+  "championId": number;
+  "detectedTeamPosition": string;
+  "elo": number;
+  "eloChange": number;
+  "gameId": number;
+  "isReportingDisabled": false;
+  "items": number[];
+  "leaver": number;
+  "leaves": number;
+  "level": number;
+  "losses": number;
+  "profileIconId": number;
+  "selectedPosition": string;
+  "skinIndex": number;
+  "skinName": string;
+  "spell1Id": number;
+  "spell2Id": number;
+  "stats": EOGStats;
+  "summonerId": number;
+  "summonerName": string;
+  "teamId": number;
+  "userId": number;
+  "wins": number;
+}
