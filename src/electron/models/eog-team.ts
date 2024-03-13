5b2cb383b2d84416a1b167538508bff50103096e
Adam Koewler <xadamxk@gmail.com>
2024-03-12 22:08:44 -0500

    format project

--- src/electron/models/eog-team.ts
+++ src/electron/models/eog-team.ts
@@ -1,17 +1,17 @@
 import { Model } from '../api';
-import { EOGPlayer } from "./eog-player";
+import { EOGPlayer } from './eog-player';
 import { EOGStats } from './eog-stats';
 
 export class EOGTeam extends Model {
-  "championBans": number[];
-  "fullId": string;
-  "isBottomTeam": boolean;
-  "isPlayerTeam": boolean;
-  "isWinningTeam": boolean;
-  "memberStatusString": string;
-  "name": string;
-  "players": EOGPlayer;
-  "stats": EOGStats;
-  "tag": string;
-  "teamId": number;
+  'championBans': number[];
+  'fullId': string;
+  'isBottomTeam': boolean;
+  'isPlayerTeam': boolean;
+  'isWinningTeam': boolean;
+  'memberStatusString': string;
+  'name': string;
+  'players': EOGPlayer;
+  'stats': EOGStats;
+  'tag': string;
+  'teamId': number;
 }
